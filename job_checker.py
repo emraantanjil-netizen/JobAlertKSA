57bf12382c5c251176dd77a6e6cd4d513b7d1ce3
emraantanjil-netizen <emraantanjil@gmail.com>
2026-08-13 01:30:08 +0300

    Add job_checker.py for Telegram job alerts

---
+++ job_checker.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "🤖 Saudi Job Alert Bot is working!"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print(response.text)
